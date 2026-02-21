3efae612b2597672ab0dcb2bcef5c909c00d22c9
Elisiane Moreira Pedrassoli <lisi1809.net@gmail.com>
2026-02-21 13:05:07 -0300

    Automação do contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,51 @@
+describe('Funcionalidade: contato', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/index.html')
+  });
+
+  it('Deve preencher formulario de contato com sucesso', () => {
+    cy.get('#name').type('Elisiane Pedrassoli')
+    cy.get('#email').type('teste@email.com')
+    cy.get('#subject').select('Parcerias')
+    cy.get('#message').type('Mensagem teste')
+    cy.get('#btn-submit').click()
+    cy.contains('Contato enviado com sucesso!').should('exist')
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem preencher nome', () => {
+    // cy.get('#name').type('Elisiane Pedrassoli')
+    cy.get('#email').type('teste@email.com')
+    cy.get('#subject').select('Parcerias')
+    cy.get('#message').type('Mensagem teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher email', () => {
+    cy.get('#name').type('Elisiane Pedrassoli')
+    // cy.get('#email').type('teste@email.com')
+    cy.get('#subject').select('Parcerias')
+    cy.get('#message').type('Mensagem teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem selecionar o assunto', () => {
+    cy.get('#name').type('Elisiane Pedrassoli')
+    cy.get('#email').type('teste@email.com')
+    // cy.get('#subject').select('Parcerias')
+    cy.get('#message').type('Mensagem teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher a mensagem', () => {
+    cy.get('#name').type('Elisiane Pedrassoli')
+    cy.get('#email').type('teste@email.com')
+    cy.get('#subject').select('Parcerias')
+    // cy.get('#message').type('Mensagem teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem')
+  });
+})
